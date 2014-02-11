e97f9f6d69b09402544a75392a3da0a4122015a1
Moritz Hanke <hanke@bips.uni-bremen.de>
2014-02-11 18:49:51 +0100

    replicate a Function in parSapply with replicate()

--- Chis_Perm_MPI.R
+++ Chis_Perm_MPI.R
@@ -344,9 +344,127 @@ system.time(
 
 
 
+####################################################################
+# replicate in parLapply
 
 
+## Wo ist der erste SNP?
+first.snp <- 2
+## Namen der SNPs
+namen <- names(phaeno.snps[,first.snp:length(phaeno.snps)])
 
+
+## Anzahl an Workern 
+numWorkers <- 2
+
+## Set up the 'cluster'
+cl <- makeCluster(numWorkers, type = "PSOCK")
+
+
+################
+#ä Hier der tricky part:
+## Wrapping a function in a function:
+
+## Funktion, die in parSapply mittels replicate x-mal ausgeführt werden soll:
+chi_functions <-function(var){
+  phaeno.snps$status <- sample(phaeno.snps$status)
+  
+  if(length(levels(as.factor((phaeno.snps[,var])))) < 3){
+    #Chi^2 for Independence 2x3
+    chi.2x3 <- try(chisq.test(xtabs(~ phaeno.snps$status + phaeno.snps[,var]),
+                              simulate.p.value = T, B=2000)$p.value)
+    
+    smallest.p.per <- chi.2x3
+    names(smallest.p.per) <- var
+    smallest.p.per
+    
+  }else{
+    n10 <- sum(na.omit(phaeno.snps[,var]==0 & phaeno.snps[,"status"]=="case"))
+    n00 <-sum(na.omit(phaeno.snps[,var]==0 & phaeno.snps[,"status"]=="control"))
+    N.0 <- n10+n00
+    
+    n11 <- sum(na.omit(phaeno.snps[,var]==1 & phaeno.snps[,"status"]=="case"))
+    n01 <- sum(na.omit(phaeno.snps[,var]==1 & phaeno.snps[,"status"]=="control"))
+    N.1 <- n11+n01
+    
+    n12 <- sum(na.omit(phaeno.snps[,var]==2 & phaeno.snps[,"status"]=="case"))
+    n02 <- sum(na.omit(phaeno.snps[,var]==2 & phaeno.snps[,"status"]=="control"))
+    N.2 <- n12+n02
+    
+    N1 <- n10 + n11 + n12
+    N0 <- n00 + n01 + n02
+    N <- N1+ N0
+    
+    
+    # Statistken für rezessives, additives(codominant) und dominates
+    # Model von:
+    # http://scholarworks.gsu.edu/cgi/viewcontent.cgi?article=1073&context=math_theses
+    
+    # Trend-Test rezessiv
+    x <- c(0,0,1)
+    
+    rec <- (N * (N*(n10*x[1]+n11*x[2]+n12*x[3]) -
+                   N1*(N.0*x[1] + N.1*x[2] +N.2*x[3]))^2)/(
+                     N1*N0*( N* (N.0*x[1]^2 + N.1*x[2]^2 +N.2*x[3]^2) -
+                               (N.0*x[1]+N.1*x[2]+N.2*x[3])^2))
+    
+    p.rec <- (1 - pchisq(rec, df=1))
+    
+    
+    # Trend-Test additiv/codominant
+    x <- c(0,1,2)
+    
+    codo <- (N * (N*(n10*x[1]+n11*x[2]+n12*x[3]) -
+                    N1*(N.0*x[1] + N.1*x[2] +N.2*x[3]))^2)/(
+                      N1*N0*( N* (N.0*x[1]^2 + N.1*x[2]^2 +N.2*x[3]^2) -
+                                (N.0*x[1]+N.1*x[2]+N.2*x[3])^2))
+    
+    p.codo <- (1 - pchisq(codo, df=1))
+    
+    
+    # Trend-Test dominant
+    x <- c(0,1,1)
+    
+    dom <- (N * (N*(n10*x[1]+n11*x[2]+n12*x[3]) -
+                   N1*(N.0*x[1] + N.1*x[2] +N.2*x[3]))^2)/(
+                     N1*N0*( N* (N.0*x[1]^2 + N.1*x[2]^2 +N.2*x[3]^2) -
+                               (N.0*x[1]+N.1*x[2]+N.2*x[3])^2))
+    
+    p.dom <- (1 - pchisq(dom, df=1))
+    
+    
+    #Chi^2 for Independence 2x3
+    
+    chi.2x3 <- try(chisq.test(xtabs(~ phaeno.snps$status + phaeno.snps[,var]),
+                              simulate.p.value = T, B=2000)$p.value)
+    
+    
+    
+    
+    smallest.p.per <- min(c(p.rec, p.codo, p.dom, chi.2x3))
+    names(smallest.p.per) <- var
+    smallest.p.per
+    
+  } #Ende else
+}
+
+
+# ClusterExport muss gesetzt werden, damit die Variabeln in
+# parLapply genutzt werden können
+clusterExport(cl=cl, varlist=c("phaeno.snps", "first.snp","namen",
+                               "chi_functions"), envir=environment())
+
+
+# with a for-loop und parSapply
+system.time(
+  chis.PER <-   parSapply(cl=cl, X=namen,
+                          FUN=function(var){
+                            replicate(50, chi_functions(var))
+                          } # Ende der Funktion
+                          
+                          ,simplify = TRUE, USE.NAMES = TRUE
+  )  # Ende parLapply
+) 
 
 
 ## Shut down cluster
